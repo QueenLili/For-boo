f38c18964b44f3b81948243f901171b41df57c3b
QueenLili <126574279+QueenLili@users.noreply.github.com>
2025-01-02 21:49:31 +0800

    Upload

---
+++ src/assets/index.js
@@ -0,0 +1,55 @@
+
+import picture1 from "./picture1.jpg";
+import picture2 from "./picture2.jpg";
+import picture from "./picture.png";
+import Together from "./Together.jpg";
+
+import heavenly from "./heavenly.jpg";
+import sparks from "./sparks.jpeg";
+import sunsetz from "./sunsetz.jpeg";
+import brandy from "./brandy.jpeg";
+import bruno from "./bruno.jpeg";
+import greenday from "./greenday.jpeg";
+import kanye from "./kanye.jpeg";
+import souljaboy from "./souljaboy.jpeg";
+import Before from "./Before.jpg";
+import Best from "./Best.jpeg";
+import Clinton from "./Clinton.jpeg";
+import Crush from "./Crush.jpg";
+import Lauren from "./Lauren.jpeg";
+import September from "./September.jpeg";
+import Sucks from "./Sucks.jpg";
+
+
+
+
+import fireworks from "./fireworks.gif";
+import christmas from "./christmas.gif";
+
+
+
+
+export {
+
+Before,
+Best,
+Clinton,
+Crush,
+Lauren,
+September,
+Sucks,
+picture1,
+picture2,
+picture,
+heavenly,
+christmas,
+fireworks,
+souljaboy,
+kanye,
+greenday,
+bruno,
+brandy,
+sunsetz,
+sparks,
+Together,
+};
